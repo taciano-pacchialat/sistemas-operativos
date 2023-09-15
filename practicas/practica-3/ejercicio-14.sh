f955401b4773049201cde959f3c6799f3bd4430c
Taciano <taciano3003@gmail.com>
2023-09-14 22:02:27 -0300

    ejercicio 14 hecho

--- practicas/practica-3/ejercicio-14.sh
+++ practicas/practica-3/ejercicio-14.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
+if [ $# -ne 3 ]; then
   echo "argumentos insuficientes"
   exit 1
 fi 
 
 directorio=$1
-caracter=$3
+opcion=$2
+cadena=$3
 
-if [  ]
+if [ ! -d "$directorio" ]; then
+  echo "El directorio $directorio no existe"
+  exit 1
+fi
+
+cd "$directorio" || exit 1
+
+for archivo in *; do
+
+  if [ -f "$archivo" ]; then
+    nombre="${archivo%.*}"
+    if [ $opcion = "-a" ]; then
+      mv ./$archivo ./${cadena}${nombre}.${archivo##*.}
+    elif [ $opcion = "-b" ]; then
+      mv ./$archivo ./${nombre}${cadena}.${archivo##*.}
+    fi
+  fi
+done
+
+exit 0
