b6a90d81afb373fac30625138779804724b5ab89
Taciano <taciano3003@gmail.com>
2023-09-29 12:44:15 -0300

    script 2 corregido

--- parcial/soa/parcial-3/script-2.sh
+++ parcial/soa/parcial-3/script-2.sh
@@ -4,6 +4,11 @@ arreglo=($(cat /etc/passwd | cut -d ':' -f 1))
 
 existe() {
 
+  if [ $# -ne 1 ]; then 
+    echo "parametros insuficientes en existe()"
+    return 2
+  fi
+
   encontrado=1
   for elemento in "${arreglo[@]}"; do
     if [ "$1" == "$elemento" ]; then
@@ -18,23 +23,34 @@ existe() {
 
 eliminar_usuario() {
 
-  if existe "$1" ; then # <- si no existe
+  if [ $# -ne 1 ]; then 
+    echo "parametros insuficientes en eliminar_usuario()"
+    return 2
+  fi
+
+  if ! existe "$1" ; then # <- si no existe
+    for ((i=0; i<${#arreglo[@]}; i++)); do
+      if [ "$1" == "${arreglo[$i]}" ]; then
+        unset arreglo[$i]
+        arreglo=($(compact ${arreglo[@]}))
+        echo "$1 eliminado"
+        break
+      fi
+    done
+  else  
     echo "$1 no existe"
     return 2
   fi
 
-  for ((i=0; i<${#arreglo[@]}; i++)); do
-    if [ "$1" == arreglo[$i] ]; then
-      unset arreglo[$i]
-      arreglo=($(compact "${arreglo[@]}"))
-      echo "usuario eliminado"
-      break
-    fi
-  done
-
 }
 
 usuarios_con_patron() {
+
+  if [ $# -ne 1 ]; then 
+    echo "parametros insuficientes en usuarios_con_patron()"
+    return 2
+  fi
+
   for elemento in "${arreglo[@]}"; do 
     if [[ "$elemento" =~ $1 ]]; then 
       echo "$elemento"
@@ -52,3 +68,9 @@ usuarios() {
   done
 }
 
+usuarios
+cantidad
+eliminar_usuario taci
+cantidad
+
+exit 0
